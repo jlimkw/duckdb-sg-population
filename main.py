47b397fabbbc0f7336037fd4e1a1021ac469c38c
Justin Lim <justinlim@Justins-MacBook-Air.local>
2026-07-02 16:27:48 +0800

    first

---
+++ main.py
@@ -0,0 +1,54 @@
+import duckdb
+import streamlit as st
+from altair.datasets import data
+
+
+# rows: Population, Resident Population, Singapore Citizen Population, Permanent Resident Population, Non-Resident Population, ...
+class DuckDBConnection:
+    def __init__(self, csv_path):
+        self.duckdb = duckdb.connect()
+        self.csv_path = csv_path
+        self.duckdb.sql(
+            f"""
+            CREATE OR REPLACE VIEW population_raw AS
+            SELECT * FROM read_csv('{self.csv_path}', header=True)
+            """
+        )
+
+    def get_population(self, data_series: tuple):
+        return self.duckdb.sql(
+            f"""
+            WITH unpivoted AS (
+                UNPIVOT (
+                    SELECT DataSeries, COLUMNS('201[5-9]|202[0-5]')
+                    FROM population_raw
+                    WHERE DataSeries IN {data_series}
+                )
+                ON COLUMNS('201[5-9]|202[0-5]')
+                INTO NAME Year VALUE Population
+            )
+            SELECT
+                DataSeries as Type,
+                Year,
+                Population
+            FROM unpivoted
+            ORDER BY Year ASC;
+            """
+        )
+
+
+def do_streamlit():
+    st.header("SG Population")
+    conn = DuckDBConnection(csv_path="IndicatorsOnPopulationAnnual.csv")
+    df = conn.get_population(
+        ("Total Population", "Resident Population", "Non-Resident Population")
+    ).pl()
+    st.line_chart(df, x="Year", y="Population", color="Type")
+    df = conn.get_population(
+        ("Singapore Citizen Population", "Permanent Resident Population")
+    ).pl()
+    st.line_chart(df, x="Year", y="Population", color="Type")
+
+
+if __name__ == "__main__":
+    do_streamlit()
